9cf4a1a24f1541d7c8551979857056ffc5f7b93a
Pietro <pietro.capriata@gmail.com>
2026-06-23 15:38:21 +0200

    debug: log click conditions for cataclysm placement

--- Game1.cs
+++ Game1.cs
@@ -401,6 +401,10 @@ public class Game1 : Game
 
         // Cataclysm placement when selected (require at least 1 frame delay after selection)
         bool cataReady = _cataSelectedFrame > 0 && (_gameFrame - _cataSelectedFrame) >= 1;
+        if (mouse.LeftButton == ButtonState.Pressed && _prevMouse.LeftButton == ButtonState.Released)
+        {
+            Logger.Debug($"[CLICK] frame={_gameFrame} cataSel={_cataclysmPanel.SelectedType} cataReady={cataReady} ptrUi={pointerOverUi} spawnCons={spawnPanelConsumed} cataCons={cataConsumed} spawnSel={_spawnPanel.SelectedCataclysm} species={_spawnPanel.SelectedSpeciesKey}");
+        }
         if (_cataclysmPanel.SelectedType != null && cataReady &&
             !pointerOverUi && !spawnPanelConsumed && !cataConsumed &&
             mouse.LeftButton == ButtonState.Pressed && _prevMouse.LeftButton == ButtonState.Released)
